e7992186adfc74734a009d2109c76f15587be369
LZL <549155088@qq.com>
2023-12-18 10:53:25 +0800

    2023/12/18更新Leetcode522,594

---
+++ src/main/java/bryanze/leetcode/leetcode522.java
@@ -0,0 +1,73 @@
+package bryanze.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 给定字符串列表 strs ，返回其中 最长的特殊序列 的长度。如果最长特殊序列不存在，返回 -1 。
+ * 特殊序列 定义如下：该序列为某字符串 独有的子序列（即不能是其他字符串的子序列）。
+ * s 的 子序列可以通过删去字符串 s 中的某些字符实现。
+ * 例如，"abc" 是 "aebdc" 的子序列，因为您可以删除"aebdc"中的下划线字符来得到 "abc" 。
+ * "aebdc"的子序列还包括"aebdc"、 "aeb" 和 "" (空字符串)。
+ *
+ * @author lizelin
+ * @date 2023/12/18
+ */
+public class leetcode522 {
+    public int findLUSLength(String[] strs) {
+
+//        Arrays.sort(strs, String::compareTo);
+
+        int result = -1;
+
+        int length = strs.length;
+
+        for (int i = 0; i < length; i++) {
+
+            /*
+            如果strs[i]的长度小于等于result，即使它不是任何字符串的子序列，
+            结果也不会保留下来，所以没必要进行下去
+             */
+            if(strs[i].length() > result){
+
+                boolean flag = true;
+
+                for (int j = 0; j < length; j++) {
+                    if (i == j) { // 不和自身检测
+                        continue;
+                    }
+
+                    //如果检查出strs[i]是某个字符串的子序列，就没必要在继续检查了，跳出循环
+                    if (isSubSequence(strs[i], strs[j])) {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag) {
+                    result = Math.max(strs[i].length(), result);
+                }
+
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isSubSequence(String str1, String str2) {
+        int p1 = 0, p2 = 0;
+        while (p1 < str1.length() && p2 < str2.length()) {
+            if (str1.charAt(p1) == str2.charAt(p2)) {
+                p1++;
+            }
+            p2++;
+        }
+
+        return p1 == str1.length();
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"aaa", "aaa", "aa"};
+        System.out.println(new leetcode522().findLUSLength(strs));
+    }
+
+}
---
+++ src/main/java/bryanze/leetcode/leetcode594.java
@@ -0,0 +1,80 @@
+package bryanze.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * 和谐数组是指一个数组里元素的最大值和最小值之间的差别 正好是 1 。
+ * 现在，给你一个整数数组 nums ，请你在所有可能的子序列中找到最长的和谐子序列的长度。
+ * 数组的子序列是一个由数组派生出来的序列，它可以通过删除一些元素或不删除元素、且不改变其余元素的顺序而得到。
+ *
+ * @author lizelin
+ * @date 2023/12/18
+ */
+public class leetcode594 {
+
+    //双指针 + 剪枝
+    public int findLHS(int[] nums) {
+
+        Arrays.sort(nums);
+        int count = 0;
+        int frequence;
+
+        for (int i = 0; i < nums.length - 1; i = i + 1 + frequence) {
+
+            boolean flag = false; //标记是否有相差为 1 的元素
+            int j = i + 1;
+            frequence = 0; // 标记重复的元素个数
+
+            for (; j < nums.length; j++) {
+
+                if (nums[j] != nums[i]) {
+
+                    if (nums[j] - nums[i] == 1) {
+                        flag = true; //更改标记
+
+                    } else {
+                        break;
+                    }
+
+                } else { //说明nums[i]与nums[j]相等，即nums[i]有重复，循环中nums[i]遍历一次即可，其他跳过
+                    frequence++;
+                }
+            }
+
+            if (flag) {
+                //此时 j 与 i 之间的都是相差为 1 的元素
+                count = Math.max(count, j - i);
+            }
+
+        }
+
+        return count;
+    }
+
+    //hash
+    public int findLHS1(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int count = 0;
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (Integer integer : map.keySet()) {
+            if(map.containsKey(integer + 1)){
+                count = Math.max(count, map.get(integer) + map.get(integer + 1));
+            }
+        }
+
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 2, 2, 5, 2, 3, 7};
+        System.out.println(new leetcode594().findLHS1(nums));
+
+    }
+}
