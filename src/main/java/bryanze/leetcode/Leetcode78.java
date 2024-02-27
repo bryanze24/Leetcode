504414dab37994ff12ba9a9b9b1c582e862c09aa
LZL <549155088@qq.com>
2024-02-27 19:44:55 +0800

    2024/2/27更新Leetcode78,90

---
+++ src/main/java/bryanze/leetcode/Leetcode78.java
@@ -0,0 +1,58 @@
+package bryanze.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+ *
+ * @author lizelin
+ * @date 2024/02/27
+ */
+public class Leetcode78 {
+
+    List<Integer> temp = new ArrayList<>();  // 存放符合条件的结果
+    List<List<Integer>> ans = new ArrayList<>(); // 存放符合条件的结果集
+
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(0, nums);
+        return ans;
+    }
+
+    private void dfs(int index, int[] nums) {
+
+        if (index == nums.length) { // 终止条件
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+
+        temp.add(nums[index]); // 遍历这个树的时候，把所有节点都记录下来，就是要求的子集集合
+        dfs(index + 1, nums);
+        temp.remove(temp.size() - 1);
+        dfs(index + 1, nums);
+
+    }
+
+
+    public List<List<Integer>> subsets1(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        list.add(new ArrayList<>());
+
+        for (int num : nums) {
+
+            int size = list.size();
+
+            for (int i = 0; i < size; i++) {
+                List<Integer> res = new ArrayList<>(list.get(i));
+                res.add(num);
+                list.add(res);
+            }
+
+        }
+
+        return list;
+    }
+
+}
---
+++ src/main/java/bryanze/leetcode/Leetcode90.java
@@ -0,0 +1,77 @@
+package bryanze.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+ * 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+ *
+ * @author lizelin
+ * @date 2024/02/27
+ */
+public class Leetcode90 {
+
+    List<List<Integer>> ans  = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    boolean[] used;
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        dfs(0, nums);
+        return ans;
+    }
+
+
+    public List<List<Integer>> subsetsWithDup1(int[] nums) {
+
+        Arrays.sort(nums);
+        used = new boolean[nums.length];
+        helper(0, nums);
+        return ans;
+    }
+
+    private void helper(int start, int[] nums) {
+        ans.add(new ArrayList<>(path));
+
+        for (int i = start; i < nums.length; i++) {
+
+            if (i > 0 && nums[i - 1] == nums[i] && !used[i - 1]) {
+                continue;
+            }
+
+            path.add(nums[i]);
+            used[i] = true;
+            helper(i + 1, nums);
+            path.removeLast();
+            used[i] = false;
+
+        }
+    }
+
+    private void dfs(int index, int[] nums) {
+        ans.add(new ArrayList<>(path));
+
+        for (int i = index; i < nums.length; i++) {
+
+            if (i > index && nums[i - 1] == nums[i]) {
+                continue;
+            }
+
+            path.add(nums[i]);
+            dfs(i + 1, nums);
+            path.removeLast();
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,2};
+        List<List<Integer>> list = new Leetcode90().subsetsWithDup1(nums);
+        for (List<Integer> integerList : list) {
+            System.out.println(integerList);
+        }
+    }
+
+}
