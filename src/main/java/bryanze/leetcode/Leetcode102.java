4d4428c4b585e8a1334086954908daafd6576b41
LZL <549155088@qq.com>
2024-05-08 14:20:49 +0800

    2024/5/8更新Leetcode102

--- src/main/java/bryanze/leetcode/Leetcode102.java
+++ src/main/java/bryanze/leetcode/Leetcode102.java
@@ -5,6 +5,7 @@ import bryanze.datastructure.queue.LinkedListQueue;
 import bryanze.datastructure.queue.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -48,6 +49,33 @@ public class Leetcode102 {
         }
 
         return result;
+    }
+
+    public List<List<Integer>> levelOrder1(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ans.add(temp);
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
